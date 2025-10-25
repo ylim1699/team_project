365511badb7e02b4764ae52d0f7494228793d641
ylim1699 <ylim.0348@gmail.com>
2025-10-25 15:13:39 -0600

    feat made footer and search form

--- fitness/src/main.js
+++ fitness/src/main.js
@@ -1,2 +1,7 @@
+const menuButton = document.querySelector("#toggleButton");
+const sideBar = document.querySelector("#side-bar");
 
+menuButton.addEventListener("click", () => {
+  sideBar.classList.remove("closed");
+});
 
