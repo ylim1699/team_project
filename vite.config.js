b42b2ded17d9a7073d9608db4dd5db6e1fbb00ce
ylim1699 <ylim.0348@gmail.com>
2025-11-13 10:38:13 -0700

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        conditions: resolve(__dirname, "src/exercises.html")
+      }
+    }
+  }
+});
