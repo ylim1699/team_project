40555095bb65eeec28a0b2a78ab863ac6aefe916
ylim1699 <ylim.0348@gmail.com>
2025-11-13 11:19:39 -0700

    apiKey

--- src/exercise.js
+++ src/exercise.js
@@ -1,10 +1,14 @@
 import { searchForExercises } from "./js_modules/search.js";
 // import { apiKey } from "../config.js";   
 const apiKey = import.meta.env.VITE_API_KEY;
-// let apiKey="PB6/7ClJ13JMeHBYWCXnHw==owpF8FRVLkR4l3pV";
+
 console.log(apiKey);
 console.log('All env vars:', import.meta.env);
 console.log('VITE_API_KEY:', import.meta.env.VITE_API_KEY);
+
+// I published it to netlify and added a environmental variable by importing
+// from .env
+// the website takes the apiKey but the local puts apiKey as undefined..
 
 
 document.getElementById("searchForm").addEventListener("submit", (e) => {
