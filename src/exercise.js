73daaba4a8317ee6eb1fdf7aff29bbb5ca0e9e89
ylim1699 <ylim.0348@gmail.com>
2025-11-13 11:16:46 -0700

    .

--- src/exercise.js
+++ src/exercise.js
@@ -1,6 +1,11 @@
 import { searchForExercises } from "./js_modules/search.js";
 // import { apiKey } from "../config.js";   
-apiKey = import.meta.env.VITE_API_KEY
+const apiKey = import.meta.env.VITE_API_KEY;
+// let apiKey="PB6/7ClJ13JMeHBYWCXnHw==owpF8FRVLkR4l3pV";
+console.log(apiKey);
+console.log('All env vars:', import.meta.env);
+console.log('VITE_API_KEY:', import.meta.env.VITE_API_KEY);
+
 
 document.getElementById("searchForm").addEventListener("submit", (e) => {
     e.preventDefault();
