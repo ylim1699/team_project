2e22482a0b6593501b3439f082685c742688c786
ylim1699 <ylim.0348@gmail.com>
2025-11-20 10:46:54 -0700

    .

--- src/exercise.js
+++ src/exercise.js
@@ -1,18 +1,12 @@
 import { searchForExercises } from "./js_modules/search.js";
-// import { apiKey } from "../config.js";   
+
 const apiKey = import.meta.env.VITE_API_KEY;
-
-console.log(import.meta.env);
-console.log(apiKey);
-console.log('VITE_API_KEY:', import.meta.env.VITE_API_KEY);
-
-// I published it to netlify and added a environmental variable by importing
-// from .env
-// the website takes the apiKey but the local puts apiKey as undefined..
-
 
 document.getElementById("searchForm").addEventListener("submit", (e) => {
     e.preventDefault();
     const search = searchForExercises(apiKey);
 });
 
+// import { apiKey } from "../config.js";   
+// I published it to netlify and added a environmental variable by importing
+// from .env
